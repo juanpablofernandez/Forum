e033a12a63aa777ec441c307e59c722e7c61ed72
Jay <jaypee.1797@gmail.com>
2016-12-12 14:40:57 -0800

    Added Usernames

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
     def check_user!
         redirect_to "/users/sign_in", notice: 'You need to be logged in to do that.' unless user_signed_in?
     end
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+        def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212192628) do
+ActiveRecord::Schema.define(version: 20161212215039) do
 
   create_table "mainforums", force: :cascade do |t|
     t.string   "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20161212192628) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
